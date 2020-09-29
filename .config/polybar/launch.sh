aaf026a9fe9d5b02f438227b5e137ee7f250633b
Iain Earl <iain@itmecho.com>
2020-09-29 15:38:05 +0100

    Remove secondary bar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,21 +6,6 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 0.1; done
 
-location=$(/home/iain/bin/whereami)
-
-# Launch bar
-case $location in
-    "work")
-        MONITOR=DP-1-2-2 polybar main &
-        MONITOR=HDMI-1-1 polybar secondary &
-        ;;
-    "home")
-        MONITOR=HDMI-1-1 polybar main &
-        MONITOR=DP-1-2-2 polybar secondary &
-        ;;
-    *)
-        polybar main &
-        ;;
-esac
+polybar main &
 
 echo "Bars launched..."
