e6f7bc5da1ba50838a88abff34f1ded309f6e331
Iain Earl <iain@itmecho.com>
2020-10-12 15:15:32 +0100

    Fix environment variables for fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,17 +1,17 @@
 #!/usr/bin/fish
 
 set fish_greeting
-
-set SHELL                  /usr/bin/fish
-set GOPATH                 $HOME/src/go
 set -gx PATH               $PATH $HOME/bin $GOPATH/bin $HOME/.local/bin $HOME/.pulumi/bin $HOME/.cargo/bin
-set EDITOR                 nvim
-set FZF_DEFAULT_COMMAND    'rg --files'
-set NAVI_PATH              $HOME/.config/cheats
-set FZF_DEAFULT_OPTS       ' --no-exact'
-set NAVI_FZF_OVERRIDES     ' --no-exact'
-set NAVI_FZF_OVERRIDES_VAR ' --no-exact'
-set GPG_TTY                (tty)
+
+export SHELL=/usr/bin/fish
+export GOPATH=$HOME/src/go
+export EDITOR=nvim
+export FZF_DEFAULT_COMMAND='rg --files'
+export NAVI_PATH="$HOME/.config/cheats"
+export FZF_DEAFULT_OPTS=' --no-exact'
+export NAVI_FZF_OVERRIDES=' --no-exact'
+export NAVI_FZF_OVERRIDES_VAR=' --no-exact'
+export GPG_TTY=(tty)
 
 alias cat    'bat -pp --theme Dracula'
 alias config '/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
