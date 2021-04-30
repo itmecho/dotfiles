760e40afe5cd780f6b9d16fd3282598865cd2c45
Iain Earl <iain@itmecho.com>
2021-04-30 12:19:36 +0100

    fish: update functions

--- fish/.config/fish/functions/tp2-up.fish
+++ fish/.config/fish/functions/tp2-up.fish
@@ -1,14 +1,24 @@
 function tp2-up
-	if test (uname) != "Darwin"
-		echo "This only works on macos"
-		return
+	if test (uname) = "Darwin"
+		echo -e "\e[01m==> Update dnsmasq config\e[00m"
+		kdev dns update-local --mimictest iain
+		echo -e "\e[01m==> Creating loopback alias\e[00m"
+		sudo ifconfig lo0 alias 127.0.0.2 up
+		echo -e "\e[01m==> Forwarding 443 to 3000\e[00m"
+		echo "rdr pass proto tcp from any to any port 443 -> 127.0.0.2 port 3000" | sudo pfctl -Ef -
+		echo -e "\e[01m==> Done\e[00m"
+		echo "    Now make sure your first DNS server in Network is set to 127.0.0.1"
+	else
+		set -l route_data (ip route get 1.1.1.1)
+		set -l local_interface (echo $route_data | awk '/1.1.1.1/ {print $5}')
+		set -l local_ip (echo $route_data | awk '/1.1.1.1/ {print $7}')
+		set -l dnsmasq_conf /etc/dnsmasq.d/sparx.conf
+		sudo rm -f $dnsmasq_conf
+		sudo touch $dnsmasq_conf
+		echo "address=/devserver.test.sparxmaths.uk/$local_ip" | sudo tee $dnsmasq_conf
+		echo "address=/.devserver.test.sparxmaths.uk/$local_ip" | sudo tee -a $dnsmasq_conf
+		sudo systemctl restart dnsmasq
+		sudo iptables -t nat -D OUTPUT --source $local_ip --destination $local_ip -p tcp --dport 443 -j REDIRECT --to-ports 3000 2>/dev/null
+		sudo iptables -t nat -A OUTPUT --source $local_ip --destination $local_ip -p tcp --dport 443 -j REDIRECT --to-ports 3000
 	end
-	echo -e "\e[01m==> Update dnsmasq config\e[00m"
-	kdev dns update-local --mimictest iain
-	echo -e "\e[01m==> Creating loopback alias\e[00m"
-	sudo ifconfig lo0 alias 127.0.0.2 up
-	echo -e "\e[01m==> Forwarding 443 to 3000\e[00m"
-	echo "rdr pass proto tcp from any to any port 443 -> 127.0.0.2 port 3000" | sudo pfctl -Ef -
-	echo -e "\e[01m==> Done\e[00m"
-	echo "    Now make sure your first DNS server in Network is set to 127.0.0.1"
 end
--- fish/.config/fish/functions/update-nvim.fish
+++ fish/.config/fish/functions/update-nvim.fish
@@ -2,7 +2,7 @@ function update-nvim
 	set -l previous_dir (pwd)
 	cd ~/src/neovim
 	git pull
-	rm -Rf build
+	git clean -x
 	make CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/.local" CMAKE_BUILD_TYPE=RelWithDebInfo
 	make install
 	cd $previous_dir
