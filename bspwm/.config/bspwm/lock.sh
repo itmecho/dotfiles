40c7415904ff8221ce167705ab0619e19bc75626
Iain Earl <iain@itmecho.com>
2021-05-10 15:58:11 +0100

    bspwm: switch i3lock to use theme colours

--- bspwm/.config/bspwm/lock.sh
+++ bspwm/.config/bspwm/lock.sh
@@ -2,12 +2,12 @@
 
 i3lock --blur=5 --force-clock --color=000000 --pass-media-keys \
     --greetercolor=928374ff \
-    --timepos="ix-0:iy-400" --time-font=LatoLight --timesize=140 --timecolor=a89984ff \
-    --datepos="tx-0:ty+90" --date-font=LatoLight --datesize=42 --datecolor=928374ff \
+    --timepos="ix-0:iy-400" --time-font=LatoLight --timesize=140 --timecolor=${THEME_FG}ff \
+    --datepos="tx-0:ty+90" --date-font=LatoLight --datesize=42 --datecolor=${THEME_FG}ff \
     --insidecolor=00000000 --insidevercolor=00000000 --insidewrongcolor=00000000 --line-uses-inside \
-    --ringcolor=282828ff --ringvercolor=458588ff --ringwrongcolor=cc241dff \
-    --keyhlcolor=b8bb26ff --bshlcolor=689d6aff \
+    --ringcolor=${THEME_BLACK}ff --ringvercolor=${THEME_BLUE}ff --ringwrongcolor=${THEME_RED}ff \
+    --keyhlcolor=${THEME_BLUE}ff --bshlcolor=${THEME_YELLOW}ff \
     --ring-width=4.0 --radius=75 \
-    --verifcolor=a89984ff --veriftext="..." \
-    --wrongtext="Incorrect" --wrongcolor=cc241dff \
+    --verifcolor=${THEME_BLUE}ff --veriftext="..." \
+    --wrongtext="Incorrect" --wrongcolor=${THEME_RED}ff \
     --noinputtext="Empty"
