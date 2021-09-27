a0e6dcd02cc9aba17bc57f1be4b2d3dddd603270
Iain Earl <iain@itmecho.com>
2021-09-27 21:43:35 +0100

    Fix update-nvim again

--- fish/.config/fish/functions/update-nvim.fish
+++ fish/.config/fish/functions/update-nvim.fish
@@ -2,9 +2,8 @@ function update-nvim
     set -l previous_dir (pwd)
     cd ~/src/neovim
     git pull
-    git clean -fx
+    git clean -dfX
     make clean
-    rm -Rf .deps
     make CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/.local" CMAKE_BUILD_TYPE=RelWithDebInfo
     make install
     cd $previous_dir
