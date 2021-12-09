3de740cf3ff92c598e00b2239e3f4e0d51ee982e
Iain Earl <iain@itmecho.com>
2021-12-09 13:19:10 +0000

    Fix tm fish function

--- fish/.config/fish/functions/tm.fish
+++ fish/.config/fish/functions/tm.fish
@@ -1,19 +1,35 @@
 function tm
     set -l create "Create session"
+    set -l kill_session "Kill session"
     set -l switch "Switch session"
 
-    set session_count (tmux list-sessions | wc -l)
+    set -l session_count (tmux list-sessions 2>/dev/null | wc -l)
 
     set -l actions
-    test $session_count -gt 0; and set -l -a actions $switch
+
+    # If there is an existing session, allow switching sessions
+    test $session_count -gt 0
+    and set -a actions $switch $kill_session
+
+    # Add the default actions
     set -l -a actions $create
+
+    # Select an action
     set action (string join \n $actions | fzf)
 
     switch $action
         case $create
             read -P 'Session name: ' name
-            tmux new-session -d -t $name
-            tmux switch-session -t $name
+            tmux new-session -d -s $name
+            if [ -z $TMUX ]
+                tmux attach-session -t $name $extra_args
+            else
+                tmux switch-client -s $name
+            end
+        case $kill_session
+            set -l session_list (tmux list-sessions -F '#S')
+            set -l session (string join \n $session_list | fzf)
+            tmux kill-session -t $session
         case $switch
             set session (tmux list-sessions -F '#S' | fzf)
             if test -z $TMUX
