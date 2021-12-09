1a4eda24a0a6b4748c5d72b2e2fd9e24489ee598
Iain Earl <iain@itmecho.com>
2021-12-09 13:18:42 +0000

    Organise aliases

--- fish/.config/fish/aliases.fish
+++ fish/.config/fish/aliases.fish
@@ -1,14 +1,10 @@
+# General
 alias bat 'bat --theme Dracula'
 alias cat 'bat -pp'
+alias cb 'xclip -i -selection clipboard'
 alias cdc 'cd $CLOUDPATH'
 alias cdcp 'cd (cd $CLOUDPATH; fd --max-depth 2 --type d | fzf)'
 alias cdn 'cd ~/Documents/notes'
-alias g git
-alias gs 'g s'
-alias gpnew 'git push -u origin (git rev-parse --abbrev-ref HEAD)'
-alias k kubectl
-alias kn kubens
-alias kx kubectx
 alias l 'ls -lh'
 alias ll 'ls -lAh'
 alias ls 'lsd --group-dirs first'
@@ -17,3 +13,23 @@ alias uuidgen "uuidgen | tr '[:upper:]' '[:lower:]'"
 alias vi nvim
 alias vim nvim
 alias work 'tmux new-session -s work -c $CLOUDPATH'
+
+# Git
+alias g git
+alias ga 'git add'
+alias gapa 'git add --interactive --patch'
+alias gb 'git switch'
+alias gc 'git commit'
+alias gco 'git checkout'
+alias gcq 'git commit --amend --no-edit'
+alias gd 'git diff'
+alias gdca 'git diff --cached'
+alias gp 'git push'
+alias gpl 'git pull'
+alias gpnew 'git push -u origin (git rev-parse --abbrev-ref HEAD)'
+alias gs 'git status'
+
+# K8s
+alias k kubectl
+alias kn kubens
+alias kx kubectx
