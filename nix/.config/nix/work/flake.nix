b69b77e144a4ce764372286ab632a990283f70a8
Iain Earl <iain@itmecho.com>
2023-05-30 10:20:53 +0100

    Switch to nix flake for work dev shell

---
+++ nix/.config/nix/work/flake.nix
@@ -0,0 +1,37 @@
+{
+    description = "Sparx";
+
+    inputs = {
+      nixpkgs.url = "nixpkgs/nixos-23.05";
+      nixpkgs_22-11.url = "nixpkgs/nixos-22.11";
+      flake-utils.url = "flake-utils";
+    };
+
+    outputs = {self, nixpkgs, nixpkgs_22-11, flake-utils} : 
+      flake-utils.lib.eachDefaultSystem 
+        (system:
+          let
+            pkgs = import nixpkgs { inherit system; };
+            pkgs_22-11 = import nixpkgs_22-11 { inherit system; };
+          in {
+            devShells.default = pkgs.mkShell {
+              buildInputs = with pkgs; [
+                python39
+                libxcrypt
+                terraform
+                kubectl
+                kubectx
+                (google-cloud-sdk.withExtraComponents [google-cloud-sdk.components.gke-gcloud-auth-plugin])
+                bazel
+                go
+                postgresql_14
+                grpcurl
+                pkgs_22-11.nodejs-16_x
+                pkgs_22-11.nodejs-16_x.pkgs.pnpm
+                jdk11
+                protobuf3_20
+              ];
+            };
+          }
+        );
+}
